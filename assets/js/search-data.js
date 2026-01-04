2e19f85a303d2cbe69d16dd408556366a08a7d75
justinjasper <125762349+justinjasper@users.noreply.github.com>
2026-01-04 20:33:08 +0000

    Deploying to gh-pages from @ justinjasper/justinjasper.github.io@cac9362943d1371d208dfc5d2cb9b20e46c915d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,15 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-icu-glucose-monitoring-optimization",
+        },{id: "projects-icu-glucose-monitoring-optimization",
           title: 'ICU Glucose Monitoring Optimization',
           description: "Offline RL + decision-theoretic modeling to recommend when to measure blood glucose in the ICU under uncertainty.",
           section: "Projects",handler: () => {
